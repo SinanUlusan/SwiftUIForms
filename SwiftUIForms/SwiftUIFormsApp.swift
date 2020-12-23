ad69c76e8cc9079d8b21c3839c66aa0fb9ed5a03
SinanStatistician <SinanStatistician@users.noreply.github.com>
2020-12-23 20:27:33 +0300

    Initial Commit

---
+++ SwiftUIForms/SwiftUIFormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormsApp.swift
+//  SwiftUIForms
+//
+//  Created by Sinan Ulusan on 23.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
